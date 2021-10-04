1ce74b65e53b6a32378beba4840b22da6cf5fc08
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2021-10-04 12:14:59 -0700

    Update InterConnection.java

--- InterConnection.java
+++ InterConnection.java
@@ -1,6 +1,12 @@
 import java.io.*;
 import java.util.ArrayList;
 
+/*
+Serialization is the conversion of the state of an object into a byte stream; 
+deserialization does the opposite. Stated differently, serialization is the conversion of a Java object into a static stream 
+(sequence) of bytes which can then be saved to a database or transferred over a network.
+*/
+
 public class InterConnection implements Serializable {
 
     private String username = null;
